6b7411ee89158e7a1b8ca727715cbda1aa6e6819
OlUwAtOmI06 <oluwatomiadenekan@gmail.com>
2026-01-10 16:49:44 -0800

    Add function to return first unique characters
    Implement function to find first occurrence of characters in a string.

--- tomi_adenekan_0/first_occurrence.py
+++ tomi_adenekan_0/first_occurrence.py
@@ -1,1 +1,17 @@
-v
+def main(mes):
+    res = ""
+    seen = set()
+    for ch in mes:
+        if ch not in seen:
+            res += ch
+        seen.add(ch)
+
+    return res
+       
+
+    
+
+message = "abracadabra"
+print(main(message))
+second = "Uber Career Prep"
+print(main(second))
