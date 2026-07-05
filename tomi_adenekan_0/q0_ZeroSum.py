615c275d80e12e399a4bfca51e9e86c821507220
OlUwAtOmI06 <oluwatomiadenekan@gmail.com>
2026-07-04 23:08:34 -0700

    Implement zeroSUM function to count zero-sum pairs
    Added a new function to count zero-sum pairs in a list.

--- tomi_adenekan_0/q0_ZeroSum.py
+++ tomi_adenekan_0/q0_ZeroSum.py
@@ -20,6 +20,28 @@ def main(array):
 
     return res
 
+def zeroSUM(lst):
+    dic = {}
+    for each in lst:
+        if each not in dic:
+            dic[each] = 1
+        else:
+            dic[each] += 1
+
+    total = 0
+    for key, val in dic.items():
+        if key < 0 and -key in dic:
+            total += (val * dic[-key])
+        if key == 0:
+            total += (val * (val -1)) // 2
+
+
+    return total
+
+
+lis = [1, 10, 8, 3, 2, 5, 7, 2, -2, -1, 0,0,0]
+print(zeroSUM(lis))
+
 lst = [1, 10, 8, -2, 2, 5, 7, 2, -2, -1]
 print(main(lst))
 
